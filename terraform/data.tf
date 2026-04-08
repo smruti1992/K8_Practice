0fc135547c7883e9cba14479e4087b3454b3fa95
jatinbhalla <jatinbhalla18.jb@gmail.com>
2026-04-08 11:56:28 +0530

    outputs.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,8 +1,19 @@
 data "aws_ami" "linux" {
   most_recent = true
+  owners      = ["099720109477"]  # Canonical (Ubuntu)
+  
   filter {
     name   = "name"
-    values = ["linux*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]  # Ubuntu 22.04 LTS
   }
   
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
-variable "ami_id" {
-  description = "The ID of the AMI to use for the EC2 instance."
-  type        = string
-}
+# variable "ami_id" {
+#   description = "The ID of the AMI to use for the EC2 instance."
+#   type        = string
+# }
 
 variable "instance_type" {
   description = "Map of instance names to instance types. Each entry creates one EC2 instance."
